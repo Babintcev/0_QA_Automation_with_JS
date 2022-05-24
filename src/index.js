971c9c3b8dcb4b352783197daea3bcc308b89122
Babintcev <74519858+Babintcev@users.noreply.github.com>
2022-05-24 12:10:45 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Dmitrii!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://74.ru" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
